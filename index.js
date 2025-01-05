bb9c1598af14b00499a9d33e4db37e792eac4c99
Sp0601 <ahmedidrissighali@gmail.com>
2025-01-05 22:10:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+ // JavaScript to switch CSS files
+ //const switchThemeButton = document.getElementById('switch-theme');
+ //const stylesheet = document.getElementById('theme-stylesheet');
+
+ //switchThemeButton.addEventListener('click', () => {
+ //  // Toggle between two CSS files
+ //  const currentTheme = stylesheet.getAttribute('href');
+ //  const newTheme = currentTheme === 'css.css' ? 'darkcss.css' : 'css.css';
+ //  const newT_heme = currentTheme === 'darkcss.css' ? 'css.css' : 'css.css';
+ //  stylesheet.setAttribute('href', newTheme);
+//});
